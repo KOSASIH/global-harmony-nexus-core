5b6b54c1b26f5e0343a20f33bf65b63457c4f150
KOSASIH <kosasihg88@gmail.com>
2024-11-05 23:13:24 +0700

    Create Proposals.js

---
+++ src/frontend/src/pages/Proposals.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Proposals = () => {
+    return (
+        <div>
+            <h1>Proposals</h1>
+            <p>List of proposals will be displayed here.</p>
+            {/* Add logic to fetch and display proposals */}
+        </div>
+    );
+};
+
+export default Proposals;
