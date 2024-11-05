068c30438a4b6d7870b6c43b044689facfbf6856
KOSASIH <kosasihg88@gmail.com>
2024-11-05 23:38:48 +0700

    Create userRoutes.js

---
+++ src/backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// routes/userRoutes.js
+const express = require('express');
+const { getUserProfile } = require('../controllers/userController');
+const { authenticate } = require('../middleware/authMiddleware');
+const router = express.Router();
+
+router.get('/profile', authenticate, getUserProfile);
+
+module.exports = router;
