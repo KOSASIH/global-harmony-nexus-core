652b867a23e6875fbb7fa64a345d3fd99d9ebf1c
KOSASIH <kosasihg88@gmail.com>
2024-11-05 23:14:04 +0700

    Create Voting.js

---
+++ src/frontend/src/pages/Voting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Voting = () => {
+    return (
+        <div>
+            <h1>Voting</h1>
+            <p>Voting interface will be implemented here.</p>
+            {/* Add logic to fetch proposals and allow voting */}
+        </div>
+    );
+};
+
+export default Voting;
